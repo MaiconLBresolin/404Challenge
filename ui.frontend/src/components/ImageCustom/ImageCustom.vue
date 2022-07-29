45c56330fc6f3336c5d94ba975a6128e9b176807
Maicon Bresolin <106255646+MaiconLBresolin@users.noreply.github.com>
2022-07-29 15:01:52 -0300

    Feat: adding feature image

---
+++ ui.frontend/src/components/ImageCustom/ImageCustom.vue
@@ -0,0 +1,24 @@
+<template>
+    <img :src="src" />
+</template>
+
+<script>
+export default {
+  name: 'ImageCustom',
+  props: {
+    src: {
+      type: String
+    }
+  }
+}
+</script>
+
+<style scoped>
+/* .imgSpace {
+    height: 100%;
+    width: 50%;
+} */
+img{
+    width: 40vw;
+}
+</style>
